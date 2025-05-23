3a01709a9f88379bac790dd95ab06c7b53fe1e27
Bilyaminu Ahmad <ahmedbilyameen01@gmail.com>
2025-05-23 23:28:37 +0100

    MILESTONE 2 - adding comment to DB connection

--- config/db.js
+++ config/db.js
@@ -5,13 +5,13 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URL);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`); //I just learned how to fetch the out the connection stream (host)
 
     } catch (error) {
         
         console.error("Database connection failed:", error.message);
 
-        process.exit(1);
+        process.exit(1); //safe exist!
     }
 };
 
